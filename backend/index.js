5c091c862297c855794106d097c9fe11fb9aa474
AbijuruT <abijuruyves@gmail.com>
2024-09-27 12:32:16 +0200

    Adding HTTP Routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import { PORT } from "./config.js";
 const app = express();
+
+//Http route
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome to MErn App')
+
+})
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 
